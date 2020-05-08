a7f855667748b9bd51bf3bbcae7496715a8796c1
Yyassin <yyass2@ocdsb.ca>
2020-05-08 13:55:18 -0400

    Website template w/some animation to build on.

---
+++ js/parallax.js
@@ -0,0 +1,42 @@
+// $(window).scroll(function() {
+//     parallax();
+// });
+
+// function parallax() {
+//     var wScroll = $(window).scrollTop();
+
+//     $('.parallax--bg').css('background-position', 
+//     'center ' + (wScroll*0.55) + 'px')
+
+//     $('.parallax--bg2').css('background-position', 
+//     'center ' + (wScroll*0.55 - 650)*-1 + 'px')
+// }
+
+$(window).scroll(function(e) {
+    parallax();
+  })
+  
+  
+  function parallax() {
+    var w = window.innerWidth;
+    console.log(w);
+    pFactor = 0.4
+
+    if (w< 1100) {
+        pFactor = 0;
+    }
+
+    var scroll = $(window).scrollTop();
+    var screenHeight = $(window).height();
+  
+    $('.parallax--bg').each(function() {
+      var offset = $(this).offset().top;
+      var distanceFromBottom = offset - scroll - screenHeight
+            
+      if (offset > screenHeight && offset) {
+        $(this).css('background-position', 'center ' + (( distanceFromBottom ) * (pFactor)) +'px');
+      } else {
+        $(this).css('background-position', 'center ' + (( scroll ) * 0.5) + 'px');
+      }
+    })
+  }
